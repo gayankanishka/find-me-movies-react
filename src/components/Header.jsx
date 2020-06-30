f901c76fe7a404a5c04fbdfff16fd163b84a5109
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-06-30 21:57:46 +0530

    feat: adding header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+const Header = () => {
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5">Find Me Movies</Typography>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Header;
