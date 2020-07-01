e188e478d91a0451f1df677bea9cdbad26bdaab4
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-07-01 23:25:42 +0530

    feat: adding popular, top rated and upcmoing moview modules to layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Layout from "./components/Layout";
-import MovieList from "./modules/featured-movies/components/MovieList";
+import PopularMovies from "./modules/movies/components/PopularMovies";
+import TopRatedMovies from "./modules/movies/components/TopRatedMovies";
+import UpcomingMovies from "./modules/movies/components/UpcomingMovies";
 
 function App() {
   return (
     <div>
       <Layout title="Home">
-        <MovieList />
+        <PopularMovies />
+        <TopRatedMovies />
+        <UpcomingMovies />
       </Layout>
     </div>
   );
