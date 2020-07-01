f8390136390394a096df188b9037dc0f6d4ebfae
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-07-01 23:16:23 +0530

    feat: adding get calls for top and upcoming movies

--- src/services/movie-db.service.js
+++ src/services/movie-db.service.js
@@ -1,9 +1,19 @@
 const { apiService } = require("./_shared/api.service");
 
 export const movieService = {
-  getTrendingMovies,
+  getPopularMovies,
+  getTopRatedMovies,
+  getUpcomingMovies,
 };
 
-function getTrendingMovies() {
+function getPopularMovies() {
   return apiService.get("movie/popular").then((res) => res.results);
+}
+
+function getTopRatedMovies() {
+  return apiService.get("/movie/top_rated").then((res) => res.results);
+}
+
+function getUpcomingMovies() {
+  return apiService.get("/movie/upcoming").then((res) => res.results);
 }
