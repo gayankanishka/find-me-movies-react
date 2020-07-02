577b34e4387be8c96c0dc427af83f414f42c6d64
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-07-02 23:11:34 +0530

    refactor: getting rid of react helmet usage

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,14 +2,10 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Container from "@material-ui/core/Container";
-import { Helmet } from "react-helmet";
 
-const Layout = ({ children, title }) => {
+const Layout = ({ children }) => {
   return (
     <>
-      <Helmet titleTemplate="Find Me Movies - %s" defaultTitle="Find Me Movies">
-        <title>{title}</title>
-      </Helmet>
       <Header />
       <Container>{children}</Container>
       <Footer />
