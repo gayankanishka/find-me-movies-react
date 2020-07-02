2f224588d7396fd129b8c6f6a6e38496bf982b7e
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-07-02 23:10:48 +0530

    feat: moving home to component

---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PopularMovies from "./modules/movies/components/PopularMovies";
+import TopRatedMovies from "./modules/movies/components/TopRatedMovies";
+import UpcomingMovies from "./modules/movies/components/UpcomingMovies";
+
+const Home = () => {
+  return (
+    <>
+      <PopularMovies />
+      <TopRatedMovies />
+      <UpcomingMovies />
+    </>
+  );
+};
+
+export default Home;
