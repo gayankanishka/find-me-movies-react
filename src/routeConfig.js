8ce7a4881472380b4cb961b0134752ec1e73d0a1
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-07-02 23:40:40 +0530

    feat: adding initial route configs

---
+++ src/routeConfig.js
@@ -0,0 +1,21 @@
+import Home from "./Home";
+import MovieDetails from "./modules/movies/components/MovieDetails";
+import PageNotFound from "./PageNotFound";
+
+export const routes = [
+  {
+    path: "/",
+    exact: true,
+    component: Home,
+  },
+  {
+    path: "/movie-details/:id",
+    exact: true,
+    component: MovieDetails,
+  },
+  {
+    path: "*",
+    exact: true,
+    component: PageNotFound,
+  },
+];
