17a15c2bbbd2140e2d0a866460375b4c8fafd6ff
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-07-03 10:47:20 +0530

    feat: adding `navigationService` for in app navigations
    - using navigation service for movie details

--- src/modules/movies/components/MovieCard.jsx
+++ src/modules/movies/components/MovieCard.jsx
@@ -3,6 +3,8 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardMedia from "@material-ui/core/CardMedia";
+import { navigationService } from "../../../services/navigation.service";
+import { config } from "../../../config";
 
 const useStyles = makeStyles({
   root: {
@@ -17,11 +19,13 @@ const MovieCard = ({ movie }) => {
 
   return (
     <Card className={classes.root}>
-      <CardActionArea>
+      <CardActionArea
+        onClick={() => navigationService.goToMovieDetails(movie.id)}
+      >
         <CardMedia
           component="img"
           alt="Movie poster"
-          image={process.env.REACT_APP_TMDB_IMAGE_BASE_URL + movie.poster_path}
+          image={config.tmdbApi.imageBaseUrl + movie.poster_path}
           title={movie.title}
         />
       </CardActionArea>
---
+++ src/services/navigation.service.js
@@ -0,0 +1,9 @@
+import history from "../utils/history.utils";
+
+export const navigationService = {
+  goToMovieDetails,
+};
+
+function goToMovieDetails(movieId) {
+  history.push(`/movie-details/${movieId}`);
+}
