08ecf8d76763bde1105c95b9993820dd7d046d40
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-07-04 00:09:31 +0530

    feat: new styles for the spinner

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { CircularProgress } from '@material-ui/core';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
+    position: 'fixed',
     display: 'flex',
-    '& > * + *': {
-      marginLeft: theme.spacing(2)
-    }
+    width: '100%',
+    height: '100%',
+    top: '0',
+    left: '0',
+    right: '0',
+    bottom: '0',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    zIndex: '2',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 }));
 
@@ -16,7 +24,7 @@ const Loader = () => {
 
   return (
     <div className={classes.root}>
-      <CircularProgress />
+      <CircularProgress size={50} thickness={5} />
     </div>
   );
 };
