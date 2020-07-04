58ee9afccb902395f3e045ae9c61dc4debbc6053
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-07-04 13:21:13 +0530

    feat: adding screen spliting for the `movie details page`

--- src/modules/movies/components/MovieDetails.jsx
+++ src/modules/movies/components/MovieDetails.jsx
@@ -1,8 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import { Card } from '@material-ui/core';
 import movieService from '../../../services/movie-db.service';
+import config from '../../../config';
+import Loader from '../../../components/Loader';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    [theme.breakpoints.down('sm')]: {
+      'flex-direction': 'column'
+    }
+  },
+  cardWrapper: {
+    maxWidth: 330,
+    padding: '1rem',
+    flex: 1
+  },
+  contentWrapper: {
+    flex: 1,
+    padding: '1rem'
+  },
+  imageWrapper: {
+    maxWidth: 300
+  }
+}));
 
 const MovieDetails = () => {
+  const classes = useStyles();
   const [movie, setMovie] = useState();
   const { id } = useParams();
 
@@ -10,7 +36,28 @@ const MovieDetails = () => {
     movieService.getMovieById(id).then((data) => setMovie(data));
   }, [id]);
 
-  return <h1>{movie ? movie.title : null}</h1>;
+  if (movie === undefined) {
+    return <Loader />;
+  }
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.cardWrapper}>
+        <Card elevation={5}>
+          <img
+            className={classes.imageWrapper}
+            src={config.tmdbApi.imageBaseUrl + movie.poster_path}
+            alt="poster"
+          />
+        </Card>
+      </div>
+      <div className={classes.contentWrapper}>
+        <h1>{movie.title}</h1>
+        <h1>{movie.release_date}</h1>
+        <p>{movie.overview}</p>
+      </div>
+    </div>
+  );
 };
 
 export default MovieDetails;
