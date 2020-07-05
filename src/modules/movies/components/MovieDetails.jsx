a84c7d1c7b1b6f333502d5c0e7f95478e0e24f34
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-07-05 23:27:42 +0530

    refactor: restructuring the movie details component - cleanup - breaking into chuncks

--- src/modules/movies/components/MovieDetails.jsx
+++ src/modules/movies/components/MovieDetails.jsx
@@ -1,35 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, Typography } from '@material-ui/core';
-import Rating from '@material-ui/lab/Rating';
+import { Grid } from '@material-ui/core';
 import movieService from '../../../services/movie-db.service';
-import config from '../../../config';
 import Loader from '../../../components/Loader';
+import MoviePoster from './MoviePoster';
+import MovieRating from './MovieRating';
+import MovieGenres from './MovieGenres';
+import MovieTitle from './MovieTitle';
+import InlineDescriptor from '../../../components/InlineDescriptor';
+import Paragraph from '../../../components/Paragraph';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    [theme.breakpoints.down('sm')]: {
-      'flex-direction': 'column'
-    }
-  },
-  cardWrapper: {
-    maxWidth: 330,
-    padding: '1rem',
-    flex: 1
+    flexGrow: 1
   },
   contentWrapper: {
-    flex: 1,
-    padding: '1rem'
-  },
-  imageWrapper: {
-    maxWidth: 300
-  },
-  titleWrapper: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center'
+    padding: theme.spacing(2)
   }
 }));
 
@@ -37,9 +24,6 @@ const MovieDetails = () => {
   const classes = useStyles();
   const [movie, setMovie] = useState();
   const { id } = useParams();
-
-  console.log(movie);
-  // convert native html elements to material elements
 
   useEffect(() => {
     movieService.getMovieById(id).then((data) => setMovie(data));
@@ -50,54 +34,20 @@ const MovieDetails = () => {
   }
 
   return (
-    <div className={classes.root}>
-      <div className={classes.cardWrapper}>
-        <Card elevation={5}>
-          <img
-            className={classes.imageWrapper}
-            src={config.tmdbApi.imageBaseUrl + movie.poster_path}
-            alt="poster"
-          />
-        </Card>
-      </div>
-      <div className={classes.contentWrapper}>
-        <div className={classes.titleWrapper}>
-          <Typography variant="h4">{movie.title}</Typography>
-          <Typography variant="h4">
-            &nbsp;(
-            {movie.release_date.split('-')[0]}
-            )&nbsp;
-          </Typography>
-        </div>
-        <div className={classes.titleWrapper}>
-          <Typography variant="h6">Genres: </Typography>
-          {movie.genres.map((genre, index) => {
-            return (
-              <Typography variant="overline" component="h3" key={genre.id}>
-                &nbsp;
-                {genre.name}
-                {index === movie.genres.length - 1 ? '' : ', '}
-              </Typography>
-            );
-          })}
-        </div>
-        <div className={classes.titleWrapper}>
-          <Typography variant="h6" display="block">
-            Rating:&nbsp;
-          </Typography>
-          <Rating
-            name="disabled"
-            value={movie.vote_average}
-            max={10}
-            precision={0.5}
-            disabled
-          />
-        </div>
-        <Typography variant="body1" paragraph>
-          {movie.overview}
-        </Typography>
-      </div>
-    </div>
+    <Grid container direction="row" className={classes.root}>
+      <MoviePoster path={movie.poster_path} />
+      <Grid item xs={12} sm={6} className={classes.contentWrapper}>
+        <MovieTitle title={movie.title} releaseDate={movie.release_date} />
+        <MovieGenres genres={movie.genres} />
+        <MovieRating voteAvg={movie.vote_average} />
+        <InlineDescriptor title="Runtime" description={movie.runtime} />
+        <InlineDescriptor
+          title="Release date"
+          description={movie.release_date}
+        />
+        <Paragraph body={movie.overview} />
+      </Grid>
+    </Grid>
   );
 };
 
