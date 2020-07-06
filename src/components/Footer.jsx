73267755d4886cc635e040c993f13d1456d0fdfd
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-07-06 23:46:07 +0530

    feat: adding initial changes to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flex: '0 0 auto',
+    backgroundColor: 'white'
+  }
+}));
 
 const Footer = () => {
-  return <>Footer</>;
+  const classes = useStyles();
+
+  return (
+    <>
+      <footer className={classes.root}>
+        <h4>Gayan K</h4>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
