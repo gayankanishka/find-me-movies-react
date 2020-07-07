6120963df3614976dedfe21327c7227bc93e6449
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-07-07 23:51:47 +0530

    feat: render movie title and rating ontop of the carousel

--- src/modules/movies/components/MovieCarousel.jsx
+++ src/modules/movies/components/MovieCarousel.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Grid, makeStyles } from '@material-ui/core';
+import { Grid, makeStyles, Paper } from '@material-ui/core';
 import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import PropTypes from 'prop-types';
 import 'swiper/swiper-bundle.css';
 import config from '../../../config';
 import navigationService from '../../../services/navigation.service';
+import MovieTitle from './MovieTitle';
+import MovieRating from './MovieRating';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,6 +19,14 @@ const useStyles = makeStyles((theme) => ({
   image: {
     maxHeight: '100%',
     maxWidth: '100%'
+  },
+  title: {
+    top: '50%',
+    left: '50%',
+    position: 'absolute',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: 'rgba(255, 255, 255, 0.6)',
+    backdropFilter: 'blur(5px)'
   }
 }));
 
@@ -43,6 +53,13 @@ const MovieCarousel = ({ movies }) => {
                 className={classes.swipeSlide}
                 onClick={() => navigationService.goToMovieDetails(movie.id)}
               >
+                <Paper elevation={3} className={classes.title}>
+                  <MovieTitle
+                    title={movie.title}
+                    releaseDate={movie.release_date}
+                  />
+                  <MovieRating voteAvg={movie.vote_average} />
+                </Paper>
                 <img
                   alt={movie.title}
                   src={config.tmdbApi.backdropBaseUrl + movie.backdrop_path}
