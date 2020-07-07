6fa2d56018e6285d4b8d828be60c443c7459841c
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-07-07 23:58:13 +0530

    feat: add navigation link to homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <AppBar position="sticky">
       <Toolbar>
-        <Typography variant="h5">Find Me Movies</Typography>
+        <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
+          <Typography variant="h5">Find Me Movies</Typography>
+        </Link>
       </Toolbar>
     </AppBar>
   );
