9ab58bf9bba7c5dd39287aaead011d62df23eb38
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-07-07 22:11:15 +0530

    feat: adding discover component into home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import PopularMovies from './modules/movies/components/PopularMovies';
 import TopRatedMovies from './modules/movies/components/TopRatedMovies';
 import UpcomingMovies from './modules/movies/components/UpcomingMovies';
+import DiscoverMovies from './modules/movies/components/DiscoverMovies';
 
 const Home = () => {
   document.getElementById('root').style.backgroundImage = null;
 
   return (
     <>
+      <DiscoverMovies />
       <PopularMovies />
       <TopRatedMovies />
       <UpcomingMovies />
