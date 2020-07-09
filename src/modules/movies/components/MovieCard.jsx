33a8651f4e6c1c7855fa7b987f5844561fa5898a
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-07-09 12:22:21 +0530

    feat: display rating on the card

--- src/modules/movies/components/MovieCard.jsx
+++ src/modules/movies/components/MovieCard.jsx
@@ -4,6 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardMedia from '@material-ui/core/CardMedia';
 import { Grid, Typography } from '@material-ui/core';
+import Rating from '@material-ui/lab/Rating';
 import PropTypes from 'prop-types';
 import navigationService from '../../../services/navigation.service';
 import config from '../../../config';
@@ -40,9 +41,17 @@ const MovieCard = ({ movie }) => {
           <Typography variant="subtitle1" align="center">
             <strong>{movie.title}</strong>
           </Typography>
-          <Typography variant="body2" align="center" fontStyle="italic">
+          <Typography variant="body2" align="center">
             {stringUtils.getDateString(movie.release_date)}
           </Typography>
+          <Grid container justify="center">
+            <Rating
+              name="disabled"
+              value={movie.vote_average / 2}
+              precision={0.5}
+              disabled
+            />
+          </Grid>
         </CardActionArea>
       </Card>
     </Grid>
@@ -54,7 +63,8 @@ MovieCard.propTypes = {
     id: PropTypes.number.isRequired,
     poster_path: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    release_date: PropTypes.string.isRequired
+    release_date: PropTypes.string.isRequired,
+    vote_average: PropTypes.number.isRequired
   }).isRequired
 };
 
