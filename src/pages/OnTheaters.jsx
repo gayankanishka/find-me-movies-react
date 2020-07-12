7a1cae9c1ab43904244e8cc7b548353f7037e4fd
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-07-12 20:04:42 +0530

    feat: initial changes for the on thearters movie component

--- src/pages/OnTheaters.jsx
+++ src/pages/OnTheaters.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import movieService from '../services/movie-db.service';
+import Loader from '../components/Loader';
+import VerticalMovieList from '../modules/movies/components/VerticalMovieList';
 
 const OnTheaters = () => {
-  return <>OnTheaters</>;
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    movieService.getNowPlayingMovies().then((data) => setMovies(data.results));
+  }, []);
+
+  if (movies === undefined) {
+    return <Loader />;
+  }
+
+  return (
+    <>
+      <Typography variant="h6">On Theaters</Typography>
+      <VerticalMovieList movies={movies} />
+    </>
+  );
 };
 
 export default OnTheaters;
