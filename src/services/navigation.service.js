a45e4756a2ac334aeec4b18a4e68319b7c164dd4
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-07-12 17:49:15 +0530

    feat(navigationService): intoducing new navigation paths

--- src/services/navigation.service.js
+++ src/services/navigation.service.js
@@ -1,11 +1,36 @@
 import history from '../utils/history.utils';
 
+function goToHome() {
+  history.push('/');
+}
+
 function goToMovieDetails(movieId) {
   history.push(`/movie-details/${movieId}`);
 }
 
+function goToOnTheaters() {
+  history.push('on-theaters');
+}
+
+function goToPopularMovies() {
+  history.push('popular-movies');
+}
+
+function goToTopMovies() {
+  history.push('top-movies');
+}
+
+function goToUpcomingMovies() {
+  history.push('upcoming-movies');
+}
+
 const navigationService = {
-  goToMovieDetails
+  goToHome,
+  goToMovieDetails,
+  goToOnTheaters,
+  goToPopularMovies,
+  goToTopMovies,
+  goToUpcomingMovies
 };
 
 export default navigationService;
