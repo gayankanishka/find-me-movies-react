90ad5730dc1f58a1bcc901629df394393c67e57e
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-07-13 23:59:41 +0530

    feat: integrating infinite scrolling for now playing movies page

--- src/pages/OnTheaters.jsx
+++ src/pages/OnTheaters.jsx
@@ -1,24 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import { Typography } from '@material-ui/core';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import movieService from '../services/movie-db.service';
-import Loader from '../components/Loader';
 import VerticalMovieList from '../modules/movies/components/VerticalMovieList';
 
 const OnTheaters = () => {
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    movieService.getNowPlayingMovies().then((data) => setMovies(data.results));
+    movieService.getNowPlayingMovies().then((data) => {
+      setMovies(data.results);
+      setPage(data.page);
+    });
   }, []);
 
-  if (movies === undefined) {
-    return <Loader />;
-  }
-
+  const fetchMovies = () => {
+    movieService.getNowPlayingMovies(page + 1).then((data) => {
+      setMovies((prev) => [...prev, ...data.results]);
+      setPage(data.page);
+    });
+  };
+  // TODO: integrate spinner for loading
   return (
     <>
       <Typography variant="h6">On Theaters</Typography>
-      <VerticalMovieList movies={movies} />
+      <InfiniteScroll
+        dataLength={movies.length}
+        next={fetchMovies}
+        hasMore={page !== 1000}
+        loader={<h4>Loading...</h4>}
+      >
+        <VerticalMovieList movies={movies} />
+      </InfiniteScroll>
     </>
   );
 };
