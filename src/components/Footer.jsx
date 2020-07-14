5d18afa42f86b6b6fd1ddb2725ae114caaf440b7
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-07-15 00:09:39 +0530

    feat: making the footer sticky bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 
-const useStyles = makeStyles(() => ({
-  root: {
-    flex: '0 0 auto',
-    backgroundColor: 'white'
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary">
+      {'Copyright © '}
+      <Link color="inherit" href="https://gayankanishka.github.io/">
+        Visit Me
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: theme.spacing(2),
+    backgroundColor: theme.palette.grey[900]
   }
 }));
 
@@ -13,8 +30,11 @@ const Footer = () => {
 
   return (
     <>
-      <footer className={classes.root}>
-        <h4>Gayan K</h4>
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <Typography variant="body1">Gayan K.</Typography>
+          <Copyright />
+        </Container>
       </footer>
     </>
   );
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import Header from './Header';
 import Footer from './Footer';
 
+const useStyles = makeStyles(() => ({
+  root: {
+    minHeight: '100vh'
+  }
+}));
+
 const Layout = ({ children }) => {
+  const classes = useStyles();
+
   return (
     <>
       <Header />
-      <Container maxWidth="xl">{children}</Container>
+      <Container maxWidth="xl" className={classes.root}>
+        {children}
+      </Container>
       <Footer />
     </>
   );
