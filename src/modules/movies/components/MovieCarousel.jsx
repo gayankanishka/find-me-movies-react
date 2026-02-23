47a66637adc14b8a31329f17b09d9c8cde60b8b2
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2026-02-23 09:16:42 +0530

    refactor: Update Swiper to v8, adjust module imports and CSS, and enable React StrictMode.

--- src/modules/movies/components/MovieCarousel.jsx
+++ src/modules/movies/components/MovieCarousel.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Grid, makeStyles } from '@material-ui/core';
-import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import PropTypes from 'prop-types';
-import 'swiper/swiper-bundle.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import config from '../../../config';
 import navigationService from '../../../services/navigation.service';
 
@@ -20,8 +22,6 @@ const useStyles = makeStyles(() => ({
   }
 }));
 
-SwiperCore.use([Navigation, Pagination, Autoplay]);
-
 function MovieCarousel({ movies }) {
   const classes = useStyles();
 
@@ -29,6 +29,7 @@ function MovieCarousel({ movies }) {
     <Grid container spacing={3}>
       <Grid item xs={12}>
         <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
           slidesPerView={1}
           autoplay={{
             delay: 10000
